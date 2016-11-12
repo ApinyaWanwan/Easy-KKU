af1591f36a285bd682813e92304a9130ab0fdc95
Apinya <Numwan30@gmail.com>
2016-11-12 15:59:36 +0700

    set path

--- app/src/main/java/com/kku/namwan/easykku/SignUpActivity.java
+++ app/src/main/java/com/kku/namwan/easykku/SignUpActivity.java
@@ -1,9 +1,11 @@
 package com.kku.namwan.easykku;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,7 +22,7 @@ public class SignUpActivity extends AppCompatActivity {
             userEditText, passwordEditText;
     private ImageView imageView;
     private Button button;
-    private String nameString, phoneString, userString, passwordString;
+    private String nameString, phoneString, userString, passwordString, imagePathString, imageNameString;
     private Uri uri;
 
 
@@ -100,8 +102,33 @@ public class SignUpActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
+            //Find Path of Image
+            imagePathString = myFindPath(uri);
+            Log.d("12novV1", "imagePath ==>" + imagePathString);
+
+
+
         }  // if
 
 
     } // onActivity
+
+    private String myFindPath(Uri uri) {
+
+        String result = null;
+        String[] strings = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result = cursor.getString(index);
+
+        } else {
+            result = uri.getPath(); //ถ้าไม่รูป ถ่ายรูปใหม่ ไม่ต้องเลือก
+        }
+
+
+        return result;
+    }
 } // Main Class
